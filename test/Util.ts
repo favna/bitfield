98f66ea7a2932858b298356b4f0e2515bf51f7cc
bdistin <bdistin@gmail.com>
2020-03-07 18:52:43 -0600

    add initial bitfield util, tests to come later

--- test/Util.ts
+++ test/Util.ts
@@ -1,6 +1,24 @@
 import ava from 'ava';
-import util from '../dist';
+import { BitField, BitFieldObject } from '../dist';
 
-ava('sample test', (test): void => {
-	test.is(util(), 'bar');
+type TestResolvable = keyof typeof TestBits.FLAGS | number | BitFieldObject | ((keyof typeof TestBits.FLAGS) | number | BitFieldObject)[]
+
+/* eslint-disable no-bitwise */
+
+class TestBits extends BitField<TestResolvable> {
+
+	static FLAGS = {
+		A: 1 << 0,
+		B: 1 << 1,
+		C: 1 << 2
+	} as const;
+
+}
+
+/* eslint-enable no-bitwise */
+
+ava('simple has', (test): void => {
+	const testBits = new TestBits(1);
+
+	test.true(testBits.has(1));
 });
